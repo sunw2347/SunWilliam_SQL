2f039c1b2522e1db8bca8523a6101dbccd2b3617
sunw2347 <williamsun2347@gmail.com>
2018-05-22 10:11:13 -0700

    SQL

---
+++ app/src/main/java/com/example/sunw2347/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,39 @@
+package com.example.sunw2347.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public static final int DATABASE_VERSION  = 1;
+    public static final String DATABASE_NAME = "Contact2018.db";
+    public static final String TABLE_NAME = "Contact2018_table";
+    public static final String ID = "ID";
+    public static final String COLUMN_NAME_CONTACT = "contact";
+
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME_CONTACT + " TEXT)";
+
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d("MyContactApp", "DatabaseHelper: constructed DatabaseHelper");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("MyContactApp", "DatabaseHelper: creating database");
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d("MyContactApp", "DatabaseHelper: upgrading database");
+        db.execSQL(SQL_DELETE_ENTRIES);
+
+    }
+}
