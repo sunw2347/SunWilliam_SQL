dbfc8bb0d2550af473f9c375bb0b71885f70e008
sunw2347 <williamsun2347@gmail.com>
2018-05-24 10:10:47 -0700

    SQL

--- app/src/main/java/com/example/sunw2347/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/sunw2347/mycontactapp/DatabaseHelper.java
@@ -13,9 +13,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "Contact2018_table";
     public static final String ID = "ID";
     public static final String COLUMN_NAME_CONTACT = "contact";
-    public static final String ADDRESS = "Address";
-    public static final long PHONE_NUMBER = 0;
-    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME_CONTACT + " TEXT)";
+    public static final String COLUMN_ADDRESS_CONTACT = "address";
+    public static final String COLUMN_PHONE_CONTACT = "phone";
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME_CONTACT + " TEXT," + COLUMN_ADDRESS_CONTACT + " TEXT," + COLUMN_PHONE_CONTACT + " TEXT)";
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     public DatabaseHelper(Context context) {
@@ -33,25 +33,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.d("MyContactApp", "DatabaseHelper: upgrading database");
         db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
 
     }
 
-    public boolean insertData(String name){
+    public int insertData(String name, String address, String phone){
         Log.d("MyContactApp", "DatabaseHelper: inserting data");
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_NAME_CONTACT, name);
-        contentValues.put(ADDRESS, name);
-        contentValues.put(name, PHONE_NUMBER);
+        contentValues.put(COLUMN_ADDRESS_CONTACT, address);
+        contentValues.put(COLUMN_PHONE_CONTACT, phone);
 
         long result = db.insert(TABLE_NAME, null, contentValues);
         if (result == -1){
             Log.d("MyContactApp", "DatabaseHelper: Contact insert = FAILED");
-            return false;
+            return 0;
         }
         else{
-            Log.d("MyContactApp", "DatabaseHelper: Contact insert = PASSED")
-            return true;
+            Log.d("MyContactApp", "DatabaseHelper: Contact insert = PASSED");
+            return 1;
         }
     }
 }
--- app/src/main/java/com/example/sunw2347/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/sunw2347/mycontactapp/MainActivity.java
@@ -3,18 +3,44 @@ package com.example.sunw2347.mycontactapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
+    EditText editName;
+    EditText editAddress;
+    EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editName = findViewById(R.id.editText_name);
+        editAddress = findViewById(R.id.editText_address);
+        editPhone = findViewById(R.id.editText_phone);
+
         myDb = new DatabaseHelper(this);
         Log.d("MyContactApp", "MainActivity: instantiated myDb");
 
     }
+
+    public void addData(View view){
+        Log.d("MyContactApp", "MainActivity: add contact button pressed");
+
+        int isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editPhone.getText().toString());
+        if(isInserted == 1){
+            Log.d("MyContactApp", "MainActivity: contact inserted");
+            Toast.makeText(MainActivity.this, "Success - contact inserted", Toast.LENGTH_LONG);
+        }
+        else{
+            Log.d("MyContactApp", "MainActivity: contact not inserted");
+            Toast.makeText(MainActivity.this, "Failed - contact not inserted", Toast.LENGTH_LONG);
+
+        }
+    }
+
 }
